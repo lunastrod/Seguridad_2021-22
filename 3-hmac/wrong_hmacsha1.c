0524e8a3ffeb5731f76bb2b4704c72b20bd0e89c
danparseg <daniel.parra.segovia@gmail.com>
2021-11-18 12:49:39 +0100

    mmmmm

--- 3-hmac/wrong_hmacsha1.c
+++ 3-hmac/wrong_hmacsha1.c
@@ -5,39 +5,22 @@
 
 enum{
     MAX_KEY_SIZE=1024,
-    MAX_DATA_SIZE=1024,
-    BLOCK_SIZE=64
+    MAX_COMMAND_SIZE=1024
 };
 
-/*
-def hmac_md5(key, msg):
-    if len(key) > blocksize:
-        key = md5(key).digest()
-    key = key + bytearray(blocksize - len(key))
-    o_key_pad = key.translate(trans_5C)
-    i_key_pad = key.translate(trans_36)
-    return md5(o_key_pad + md5(i_key_pad + msg).digest())
-*/
-
-void hmacsha1(int8_t * key, int key_size, int8_t * msg, int msg_size){
-    if(key_size>BLOCK_SIZE){
-        
-    }
-}
-
 int main(int argc, char **argv) {
-    /*
     if(argc!=3){
         fprintf(stderr,"argc error\n");
         fprintf(stderr,"usage: hmacsha1 data_file key_file\n");
         return -1;
     }
     //argv[1] data file, input to openssl
-    //argv[2] key file, extract key
+    //argv[2] key file, extract key, input to openssl
 
     FILE *key_file;
     char key[MAX_KEY_SIZE];
 
+    /* opening file for reading */
     key_file = fopen(argv[2] , "r");
     if(key_file == NULL) {
         fprintf(stderr,"Error opening key_file\n");
@@ -46,7 +29,17 @@ int main(int argc, char **argv) {
 
     printf("%s",fgets(key, MAX_KEY_SIZE, key_file));
     fclose(key_file);
-    */
 
-    return 0;
+    
+    char command[MAX_COMMAND_SIZE];
+
+    //-binary returns binary data, xxd creates a hex dump of it.
+    //I don't like the openssl dgst -hex format, it adds "(stdin)= "
+    snprintf(command,MAX_COMMAND_SIZE,"openssl dgst -sha1 -binary -hmac '%s' < %s | xxd -p",key,argv[1]);
+
+    printf("%s\n",command);
+    fflush(stdout);
+    system(command);
+
+   return 0;
 }
